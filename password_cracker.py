c1b1b6b7ff56dad790166b10d3aeb7ab62b965ba
Tom Allpress <tomallpress@hotmail.com>
2019-02-11 23:19:50 +0000

    add in begginings of breeding

--- password_cracker.py
+++ password_cracker.py
@@ -3,8 +3,9 @@ import string
 from operator import itemgetter
 
 class PasswordCracker(object):
-    def __init__(self, password):
+    def __init__(self, password, population_size):
         self.password = password
+        self.population_size = population_size
 
     def test_word(self, word_to_test):
         score = 0
@@ -19,29 +20,51 @@ class PasswordCracker(object):
                 i+=1
             return score * 100 / len(self.password)
 
-    def generate_populus(self, number_of_individuals, word_length):
+    def generate_populus(self, word_length):
         population = []
-        for i in range(number_of_individuals):
-            population.append(self.generateWord(word_length))
+        for i in range(self.population_size):
+            population.append(self.generate_word(word_length))
         return population
 
-    def generateWord(self, word_length):
+    def generate_word(self, word_length):
         # choice_of_characters = string.ascii_letters + string.digits
         choice_of_characters = string.ascii_letters
         return ''.join(random.choice(choice_of_characters)
             for _ in range(word_length))
 
-    def computePopulation(self, population):
+    def compute_population(self, population):
         calculated_population = {}
         for word in population:
             score = self.test_word(word)
             calculated_population[word] = score
         return sorted(calculated_population.items(), key = itemgetter(1), reverse=True)
 
-    def selectFromPopulation(self, population, good_sample_size, random_sample_size):
+    def select_from_population(self, population, good_sample_size, random_sample_size):
         good_samples = population[:good_sample_size]
         remaining_population = population[good_sample_size:]
         random.shuffle(remaining_population)
-        random_samples = population[:random_sample_size]
+        random_samples = remaining_population[:random_sample_size]
         concat_list = good_samples + random_samples
         return concat_list
+
+    def create_next_generation(self, population):
+        # print(len(population))
+        next_generation = []
+        for i in range(self.population_size-1):
+            index_1 = random.randint(0, len(population))
+            index_2 = random.randint(0, len(population))
+            child = self.breed(population[index_1], population[index_2])
+            next_generation.append(child)
+        return next_generation
+        # take two from population at random
+        # breed them
+
+
+    def breed(self, parent_a, parent_b):
+        child = ""
+        for i in range(len(parent_a)):
+            if random.random() < 0.5:
+                child += parent_a[i]
+            else:
+                child += parent_b[i]
+        return child
--- run.py
+++ run.py
@@ -1,6 +1,8 @@
 from password_cracker import PasswordCracker
 
-cracker = PasswordCracker('ab')
-population = cracker.generate_populus(100, 2)
-computed_population = cracker.computePopulation(population)
-print(cracker.selectFromPopulation(computed_population, 5, 1))
+cracker = PasswordCracker('ab', 100)
+population = cracker.generate_populus(2)
+computed_population = cracker.compute_population(population)
+selection = cracker.select_from_population(computed_population, 8,2)
+next_generation = cracker.create_next_generation(selection)
+print(next_generation)
